1c79f1a9c3f7e4ea2d20689efb7445fbd3898890
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-03-06 15:49:41 +0800

    toggle animation for walk and idle

--- public/Player.js
+++ public/Player.js
@@ -10,9 +10,11 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         scene.load.animation('female_anim', 'assets/images/female_anim.json');
     }
 
+    get velocity() {
+        return this.body.velocity;
+    }
+
     update() {
-    
-        this.anims.play('female_idle', true);
     
         // handle player movement speed
         const speed = 2.5;
@@ -38,6 +40,13 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         playerVelocity.normalize(); // make diagonal movements same velocity as up/down/left/right
         playerVelocity.scale(speed);
         this.setVelocity(playerVelocity.x, playerVelocity.y);
+
+        // if player is moving, play walk animation
+        if (Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
+            this.anims.play('female_walk', true);
+        } else {
+            this.anims.play('female_idle', true);
+        }
     
     }
 }
